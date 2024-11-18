5d6ffe59b32ca68c0604b34def46d504076e3bd6
Alejandra Begoña <aleromper@alu.edu.gva.es>
2024-11-18 16:32:35 +0100

    Commit de los ajustes realizados gitignore y el formulario ABRP2425

--- ejercicioTelegramaABRP2425/Form1.Designer.cs
+++ ejercicioTelegramaABRP2425/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnCalcular = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            txtTelegrama = new RichTextBox();
+            chkUrgente = new CheckBox();
+            txtPrecio = new TextBox();
+            SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(285, 246);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 0;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(80, 246);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Coste:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(80, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Texto:";
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(82, 52);
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(278, 129);
+            txtTelegrama.TabIndex = 3;
+            txtTelegrama.Text = "";
+            // 
+            // chkUrgente
+            // 
+            chkUrgente.AutoSize = true;
+            chkUrgente.Location = new Point(80, 200);
+            chkUrgente.Name = "chkUrgente";
+            chkUrgente.Size = new Size(76, 19);
+            chkUrgente.TabIndex = 4;
+            chkUrgente.Text = "Urgente ?";
+            chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(139, 243);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(100, 23);
+            txtPrecio.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(417, 343);
+            Controls.Add(txtPrecio);
+            Controls.Add(chkUrgente);
+            Controls.Add(txtTelegrama);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnCalcular);
+            Name = "Form1";
+            Text = "ABRP2425";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnCalcular;
+        private Label label1;
+        private Label label2;
+        private RichTextBox txtTelegrama;
+        private CheckBox chkUrgente;
+        private TextBox txtPrecio;
     }
 }
